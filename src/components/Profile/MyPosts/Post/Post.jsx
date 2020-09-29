a4b7a991e59c2a624a09755b1981f394ceef38cd
evdokimoww <evdokimoww@gmail.com>
2020-09-29 16:46:01 +0300

    test

--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -7,7 +7,7 @@ const Post = (props) => {
             <img src ="https://upload.wikimedia.org/wikipedia/commons/thumb/5/50/User_icon-cp.svg/1200px-User_icon-cp.svg.png"/>
             {props.text}
             <div>
-                like: {props.likeCount}
+                likes: {props.likeCount}
             </div>
         </div>
     )
